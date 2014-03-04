5222b5bf87300745eac2425dd70875770823ee80
Erwann Cloarec <erwann.cloarec@gmail.com>
2014-03-04 22:17:32 +0100

    Date

---
+++ src/fr/harmonicate/essence/DBEntry.java
@@ -0,0 +1,13 @@
+package fr.harmonicate.essence;
+
+import java.sql.Date;//Pas dit qu'il faut utiliser ca mais c'est peut etre mieux pour la BD
+
+public class DBEntry {
+
+	private String name;
+	
+	private float price;
+	
+	private Date date;//A voir
+	
+}
